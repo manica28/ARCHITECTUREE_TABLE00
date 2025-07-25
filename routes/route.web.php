7f3d62c8a37b89aa131c488a13e0c8416a10e39c
Nianga Mbaye <mbayenianga28@gmail.com>
2025-07-25 14:21:39 +0000

    architecture

---
+++ routes/route.web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Controller\CitoyenController;
+
+$routes = 
+[
+    // exemple de cas d'utilisation 
+    
+    // 'GET:/api/citoyens' => 
+    // [
+    //     'controller' => CitoyenController::class,
+    //     'method' => 'index',
+    // ],
+
+    // 'GET:/api/citoyens/{cni}' => 
+    // [
+    //     'controller' => CitoyenController::class,
+    //     'method' => 'show',
+    // ]
+];
